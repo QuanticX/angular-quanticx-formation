260fc7d2112aed0696a852d2b827f14d6ae4f631
QuanticX <mohamed.abdelnabi.fullstack@outlook.fr>
2020-03-04 10:17:48 +0100

    pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { InterpolationComponent } from './interpolation/interpolation.component';
+import { PipeComponent } from './pipe/pipe.component';
 
 const appRoutes: Routes = [
   { path: 'interpolation', component: InterpolationComponent },
+  { path: 'pipe', component: PipeComponent },
 ];
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, RouterModule.forRoot( appRoutes )
  ],
-  declarations: [ AppComponent, HelloComponent, InterpolationComponent ],
+  declarations: [ AppComponent, HelloComponent, InterpolationComponent, PipeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/pipe/pipe.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipe',
+  templateUrl: './pipe.component.html',
+  styleUrls: ['./pipe.component.css']
+})
+export class PipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  private object= {
+    element: "element",
+    type: 'type'
+  }
+
+
+}
