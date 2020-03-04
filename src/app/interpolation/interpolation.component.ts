61f9eaef4dcad6c7f74922717487c8e59850baab
QuanticX <mohamed.abdelnabi.fullstack@outlook.fr>
2020-03-04 10:03:49 +0100

    interpolation

---
+++ src/app/interpolation/interpolation.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolation',
+  templateUrl: './interpolation.component.html',
+  styleUrls: ['./interpolation.component.css']
+})
+export class InterpolationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+    private interpolation: string= 'une interpolation';
+
+
+}
