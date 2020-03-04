a2ecbfdbde8fa78f0ca000be06409704ea8a78c8
QuanticX <mohamed.abdelnabi.fullstack@outlook.fr>
2020-03-04 11:18:52 +0100

    Property Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,19 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { InterpolationComponent } from './interpolation/interpolation.component';
 import { PipeComponent } from './pipe/pipe.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 const appRoutes: Routes = [
   { path: 'interpolation', component: InterpolationComponent },
   { path: 'pipe', component: PipeComponent },
+  { path: 'property-binding', component: PropertyBindingComponent },
 ];
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, RouterModule.forRoot( appRoutes )
  ],
-  declarations: [ AppComponent, HelloComponent, InterpolationComponent, PipeComponent ],
+  declarations: [ AppComponent, HelloComponent, InterpolationComponent, PipeComponent, PropertyBindingComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
